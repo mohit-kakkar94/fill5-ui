ed5a9c5532ca6328b7dc3eaa8461f9c8a76641e6
mohit-kakkar94 <k.mohit.1694@gmail.com>
2025-01-04 01:31:28 +0530

    Added Vercel analytics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { Analytics } from "@vercel/analytics/react";
 import Game from "./ui/Game";
 
 function App() {
   return (
     <div className="h-full">
       <Game />
+      <Analytics />
     </div>
   );
 }
